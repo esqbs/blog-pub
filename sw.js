8c38f65595e5ab0a3e7b5a27fc33ffd186e8f633
esqbs <63218201+esqbs@users.noreply.github.com>
2022-05-26 18:13:26 +0800

    Deploying to gh-pages from @ esqbs/blog@f5086600ba644a6ea87361ebe049bb6abd6ed10b 🚀

--- sw.js
+++ sw.js
@@ -1,22 +1,21 @@
-let cachesName = "sw_v2"
+let cachesName = "sw_v3"
 let cachesList = [
   "/index.html"
 ]
 
 self.addEventListener('install', function(e) {
   e.waitUtil(
-    caches.open(cachesName).then(function(cache) {
-      return cache.addAll(cachesList)
-    }).then(() => {
-      self.skipWaiting()
-    })
+    caches.open(cachesName)
+    .then((cache) => cache.addAll(cachesList))
+    .then(self.skipWaiting())
   )
 })
 
 self.addEventListener('fetch', function(e) {
   if(e.request.url.startWith(self.location.origin)) {
     e.respondWith(
-      caches.match(e.request).then(function(r) {
+      caches.match(e.request)
+      .then(function(r) {
         return r || fetch(e.request).then(function(response) {
           return caches.open(cachesName).then(function(cache) {
             cache.put(e.request, response.clone())
@@ -31,16 +30,13 @@ self.addEventListener('fetch', function(e) {
 self.addEventListener('activate', function(e) {
   let cacheWhitelist = [cachesName];
   e.waitUtil(
-    caches.keys().then(function(keyList) {
+    caches.keys()
+    .then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
         if(cacheWhitelist.indexOf(key) === -1) {
           return caches.delete(key)
         }
       }))
-    }).then(
-      () => {
-        self.clients.claim()
-      }
-    )
+    }).then(() => self.clients.claim())
   )
 })
