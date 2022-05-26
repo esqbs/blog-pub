a29a7134d4b2a8bc4b9fad026bd05e1a240f154e
esqbs <63218201+esqbs@users.noreply.github.com>
2022-05-26 14:00:46 +0800

    Deploying to gh-pages from @ esqbs/blog@c4aacc4fa32cadcb7704ddf5f99f227caeb8a31e 🚀

---
+++ sw.js
@@ -0,0 +1,42 @@
+let cachesName = "sw_v1"
+let cachesList = [
+  "/index.html"
+]
+
+self.addEventListener('install', function(e) {
+  console.log('[Service Worker] Install');
+  e.waitUtil(
+    caches.open(cachesName).then(function(cache) {
+      console.log('[Service Worker] Caching all');
+      return cache.addAll(cachesList)
+    })
+  )
+})
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then(function(response) {
+        return caches.open(cachesName).then(function(cache) {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.push(e.request, response.clone())
+          return response
+        })
+      })
+    })
+  )
+})
+
+self.addEventListener('activate', function(e) {
+  let cacheWhitelist = [cachesName];
+  e.waitUtil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if(cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key)
+        }
+      }))
+    })
+  )
+})
